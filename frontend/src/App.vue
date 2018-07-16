dac8a9fb9c22f217c365ff0b008c22b8bdb00278
alex <drptau@gmail.com>
2018-07-16 11:19:08 -0400

    refine code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <div id="app">
-    <h1>Hello World!</h1>
-    <!-- <button @click="sendRequest">Get Audio</button> -->
-    <!-- <button @click="getAudioLinks">Get Audio Links</button> -->
+<div id="app">
+    <h1>App Root</h1>
     <RawAudioView></RawAudioView>
-
-  </div>
+</div>
 </template>
 
 <script>
@@ -13,43 +10,9 @@ import RawAudioView from "./components/cards/RawAudioView.vue";
 
 export default {
   name: "app",
-
   components: {
     RawAudioView
-  },
-  // sockets: {
-  //   connect: function() {
-  //     console.log("Client: get Server");
-  //   },
-
-  //   returnAudio: function(audio) {
-  //     console.log(audio);
-  //   },
-
-  //   // listen for "returnAudioLinks" emmited from backend with data "audioLinks"
-  //   returnAudioLinks: function(audioLinks) {
-  //     // console.log(audioLinks);
-
-  //     this.$store.commit("setAudioLinks", audioLinks);
-  //     console.log("From store getters getAudioLinks");
-  //     console.log(this.$store.getters.getAudioLinks);
-  //   }
-  // },
-  // methods: {
-  //   sendRequest() {
-  //     console.log("sendRequest activate");
-  //     this.$socket.emit("getAudio");
-  //   },
-
-  //   // emit "getAudioLinks" to backend server
-  //   getAudioLinks() {
-  //     console.log("getAudioLinks emitted");
-  //     this.$socket.emit("getAudioLinks");
-  //   }
-  // },
-  // created() {
-  //   this.getAudioLinks();
-  // }
+  }
 };
 </script>
 
--- frontend/src/components/cards/RawAudioView.vue
+++ frontend/src/components/cards/RawAudioView.vue
@@ -1,17 +1,11 @@
 <template>
 <div>
     <p>RawAudioViews Parent</p>
-
     <template v-for="audio in audios">
         <RawAudioViewSingle :audio='audio' :key="audio.id"></RawAudioViewSingle>
     </template>
-    <!-- <RawAudioViewSingle :audio='audios[0]'></RawAudioViewSingle> -->
-
     <br>
-    <button @click="loadMore" class="btn btn-primary">Load More</button>
-    <button @click="generateAudios" class="btn btn-primary">generateAudios</button>
-    
-
+    <button @click="loadAudios" class="btn btn-primary">Load More</button>
 </div>
 </template>
 
@@ -20,49 +14,33 @@ import RawAudioViewSingle from "./RawAudioViewSingle.vue";
 export default {
   data: function() {
     return {
-      // audioLinks: this.$store.state.audioLinks,
-      audioLinks: [],
-
-      audios: [], // populated once created, {id, link}, link in from audioLinks
-
-      numOfAudioLinks: 0, // number of audioLinks, initialize as 0
       numOfFirstLoaded: 2, // numver of audios firstly loaded
       numOfEachLoaded: 1, // numver of audios loaded each time the button is pressed
-      indexOfLastLoaded: 0 // record the index of the last loaded audio
+
+      audioLinks: [], // list of audios from backend response
+      audios: [], // audio objects, {id, audioLink} where auidoLink is from audioLinks
+
+      numOfAudioLinks: 0, // number of audioLinks totally, initialize as 0
+      indexOfLastLoaded: -1 // record the index of the last loaded audio
     };
   },
   sockets: {
     connect: function() {
-      console.log("Client: get Server");
+      console.log("Client: connect to Server");
     },
-
-    // returnAudio: function(audio) {
-    //   console.log(audio);
-    // },
 
     // listen for "returnAudioLinks" emmited from backend with data "audioLinks"
     returnAudioLinks: function(audioLinks) {
-      // console.log(audioLinks);
+      this.$store.commit("setAudioLinks", audioLinks); // update data in store
+      this.audioLinks = this.$store.state.audioLinks; // update data in this vue object
+      this.numOfAudioLinks = this.audioLinks.length; // update numOfAudioLinks
 
-      this.$store.commit("setAudioLinks", audioLinks);
-      console.log("From store getters getAudioLinks");
-      console.log(this.$store.getters.getAudioLinks);
-      this.generateAudios();
+      this.loadAudios(this.numOfFirstLoaded); // when get audioLinks from backend, load some of them
     }
   },
   methods: {
-    // sendRequest() {
-    //   console.log("sendRequest activate");
-    //   this.$socket.emit("getAudio");
-    // },
-
-    // emit "getAudioLinks" to backend server
-    getAudioLinks() {
-      console.log("getAudioLinks emitted");
-      this.$socket.emit("getAudioLinks");
-    },
-
-    loadMore() {
+    loadAudios(numToLoad = this.numOfEachLoaded) {
+      // console.log("loadAudios called");
       // if all audios have been loaded, indexOfLastLoaded is same as numOfAudioLinks-1, then return
       if (this.indexOfLastLoaded == this.numOfAudioLinks - 1) {
         console.log("No more audio to load");
@@ -70,7 +48,7 @@ export default {
       }
       var len = this.numOfAudioLinks;
       var indexLastEnd = this.indexOfLastLoaded;
-      var indexEnd = Math.min(indexLastEnd + this.numOfEachLoaded, len - 1);
+      var indexEnd = Math.min(indexLastEnd + numToLoad, len - 1);
       this.indexOfLastLoaded = indexEnd;
 
       for (var i = indexLastEnd + 1; i <= indexEnd; i++) {
@@ -79,35 +57,19 @@ export default {
           link: this.audioLinks[i]
         };
         this.audios.push(audio);
-        console.log(audio);
+        // console.log(audio);
       }
-      // console.log(this.audios.len);
     },
 
-    generateAudios() {
-      // create audio object, {id, link}, link is from audioLinks
-      // for each audio link in audioLinks, create an object with id and link
-      this.audioLinks = this.$store.state.audioLinks;
-      console.log(this.audioLinks);
-      this.numOfAudioLinks = this.audioLinks.length; // len for shortcut
-      var len = this.numOfAudioLinks;
-      console.log(len);
-      this.indexOfLastLoaded = Math.min(len, this.numOfFirstLoaded) - 1;
-      this.audios = [];
-      for (var i = 0; i <= this.indexOfLastLoaded; i++) {
-        var audio = {
-          id: i,
-          link: this.audioLinks[i]
-        };
-        this.audios.push(audio);
-      }
+    // emit "getAudioLinks" to backend server
+    getAudioLinks() {
+      // console.log("getAudioLinks emitted");
+      this.$socket.emit("getAudioLinks");
     }
   },
   created() {
     this.getAudioLinks();
-    // this.generateAudios();
   },
-
   components: {
     RawAudioViewSingle
   }
